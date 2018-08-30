681ef5678ac017e29c14f07f765189d73af31308
schemes <schemes@velocitypartners.net>
2018-08-30 11:25:09 -0300

    adds standup order randomly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  devs: any = ["gise", "renzo", "ivan", "salim"];
+  date: any = new Date();
+  constructor() {
+    this.giveMeOrder();
+  }
+  giveMeOrder() {
+    this.devs.sort(function () { return 0.5 - Math.random() })
+  }
+
 }
